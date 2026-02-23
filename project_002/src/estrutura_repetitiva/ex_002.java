d8481083d45006ae4f14f9a3e5073680b4a10ab9
samuel <samuelpereira.dev@gmail.com>
2026-02-23 16:33:41 -0300

    estruturas_repetitivas

---
+++ project_002/src/estrutura_repetitiva/ex_002.java
@@ -0,0 +1,25 @@
+package estrutura_repetitiva;
+
+import java.util.Scanner;
+
+public class ex_002 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		System.out.println("digite um numero: ");
+		n = sc.nextInt();
+		
+		int fat = 1;
+		
+		for(int i=1; i<=n; i++) {
+			fat *= i;
+		}	
+		
+		System.out.println("fatorial de " + n +  " e " + fat);
+		
+		sc.close();
+	}
+	
+	
+}
--- project_002/src/funcoes_matematicas/ex_003.java
+++ project_002/src/funcoes_matematicas/ex_003.java
@@ -8,17 +8,24 @@ public class ex_003 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-	
-	
+		
+		int numeroFuncionario, horasTrabalho;
+		double valorPorHora, result;
+		
+		System.out.println("digite o numero do funcionario: ");
+		numeroFuncionario = sc.nextInt();
+		System.out.println("digite a quantidade de horas de trabalho: ");
+		horasTrabalho = sc.nextInt();
+		System.out.println("qual o valor por hora? ");
+		valorPorHora = sc.nextDouble();
+		
+		result = horasTrabalho * valorPorHora;
+		
+		System.out.printf("NUMERO = %d %n" + "SALARIO = %.2f ",numeroFuncionario, result);
+		
+		
 		
 		
 		sc.close();
-		
-		
-		
-		
-		
-		
-		
 	}
 }
